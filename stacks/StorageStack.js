3e7aa825872331ca78b957b5045c328e007cdfbf
Edson <edsonbisneto12@gmail.com>
2022-02-27 14:56:20 -0300

    add storage and bucket using DynamoDB and AWS S3

---
+++ stacks/StorageStack.js
@@ -0,0 +1,20 @@
+import * as sst from "@serverless-stack/resources";
+
+export default class StorageStack extends sst.Stack {
+	table;
+	bucket;
+
+	constructor(scope, id, props) {
+		super(scope, id, props);
+		// Create an S3 bucket
+		this.bucket = new sst.Bucket(this, "Uploads");
+		this.table = new sst.Table(this, "Notes", {
+			fields: {
+				userId: sst.TableFieldType.STRING,
+				noteId: sst.TableFieldType.STRING,
+			},
+
+			primaryIndex: { partitionKey: "userId", sortKey: "noteId" },
+		});
+	}
+}
